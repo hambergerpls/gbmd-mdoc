dc8af8494659d72a9e57a74e90d308c83294ed09
hambergerpls <danial@hambergerpls.dev>
2024-09-29 22:28:25 +0800

    release: v0.0.5

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { gbmd_mdoc } from 'gbmd-mdoc';
 program
   .name('gbmd-mdoc')
   .description('Converts Gitbook md to Markdoc mdoc')
-  .version('0.0.4')
+  .version('0.0.5')
   .requiredOption('-o, --output <dir>', 'output directory')
   .requiredOption('-i, --input <dir>', 'input directory')
   .action(gbmd_mdoc)
