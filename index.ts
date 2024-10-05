30200989b843f2eb2a77aa765b312337f94bc170
hambergerpls <danial@hambergerpls.dev>
2024-10-05 21:03:24 +0800

    refactor!: move node dependencies to program

--- index.ts
+++ index.ts
@@ -1,7 +1,4 @@
-import { mkdirSync, readFileSync, writeFileSync } from 'node:fs';
-import { dirname } from 'node:path';
 import Markdoc, { Config, type Node, type ValidateError } from '@markdoc/markdoc';
-import { globSync } from 'glob';
 import { config as defaultConfig } from './src/markdoc.config';
 import yaml from 'js-yaml';
 import { type GitBookFrontmatter } from './src/types';
@@ -57,66 +54,7 @@ export function appendToFrontmatter(ast: Node, data: { [key: string]: any }) {
   return Markdoc.format(ast);
 }
 
-export function convertFile(filePath: string, config: Config = {}) {
-    config = { ...defaultConfig, ...config }
-    let file = readFileSync(filePath, { encoding: 'utf8', flag: 'r' }).split('\n');
 
-    let ast = Markdoc.parse(file.join('\n'));
-    let errors = Markdoc.validate(ast, config);
-    if (errors.length !== 0) {
-      // Resolve end* to /* first
-      resolveClosingTag(errors, file);
-    }
-
-    ast = Markdoc.parse(file.join('\n'));
-    errors = Markdoc.validate(ast, config);
-    if (errors.length !== 0) {
-      // Resolve self-enclosing tags
-      resolveSelfEnclosingTag(errors, file);
-    }
-
-    ast = Markdoc.parse(file.join('\n'));
-    errors = Markdoc.validate(ast, config);
-
-    file = extractTitleToFrontmatter(ast).split('\n');
-
-    return { file, errors }
-}
-
-export async function gbmd_mdoc(options: { input: string; output: string }, config: Config = defaultConfig) {
-
-  const inputPath = options.input;
-  if (!inputPath) throw new Error('Input Path is empty')
-
-  const outputPath = options.output;
-  if (!outputPath) throw new Error('Output Path is empty')
-
-  let data = "";
-  if (!process.stdin.isTTY) {
-    for await (const chunk of process.stdin) data += chunk
-  }
-
-  const filePaths = [...globSync(`${inputPath}/**/*.md`), ...data.split('\n') ];
-
-  for (const filePath of filePaths) {
-    if (!filePath){
-      continue;
-    }
-
-    let {file, errors} = convertFile(filePath, config);
-
-    if (errors.length !== 0) {
-      process.stderr.write(filePath)
-      process.stderr.write(file.join('\n'))
-      process.stderr.write(JSON.stringify(errors, undefined, 2))
-    }
-    else {
-      const dataOutput = new Uint8Array(Buffer.from(file.join('\n')))
-      mkdirSync(`${outputPath}/${dirname(filePath.replace(inputPath, ''))}`, {recursive: true});
-      writeFileSync(`${outputPath}/${filePath.replace(inputPath, '').replace(/\.md$/, '.mdoc')}`, dataOutput)
-    }
-  }
-}
 
 export {
   tags,
@@ -131,8 +69,6 @@ export default class GBMD_MDOC {
   static resolveSelfEnclosingTag = resolveSelfEnclosingTag;
   static extractTitleToFrontmatter = extractTitleToFrontmatter;
   static appendToFrontmatter = appendToFrontmatter;
-  static convertFile = convertFile;
-  static gbmd_mdoc = gbmd_mdoc;
 
   selfEnclosingTags;
   config;
@@ -141,6 +77,5 @@ export default class GBMD_MDOC {
     this.selfEnclosingTags = selfEnclosingTags;
   }
 
-  convertFile = (filePath: Parameters<typeof convertFile>[0]) => convertFile(filePath, this.config)
   resolveSelfEnclosingTag = (errors: Parameters<typeof resolveSelfEnclosingTag>[0], file: Parameters<typeof resolveSelfEnclosingTag>[1]) => resolveSelfEnclosingTag(errors, file, this.selfEnclosingTags);
 }
--- program.ts
+++ program.ts
@@ -1,7 +1,74 @@
 #!/usr/bin/env -S npx tsx
 
 import { program } from 'commander';
-import { gbmd_mdoc } from 'gbmd-mdoc';
+import { defaultConfig, resolveClosingTag, resolveSelfEnclosingTag, extractTitleToFrontmatter } from 'gbmd-mdoc';
+
+import { mkdirSync, readFileSync, writeFileSync } from 'node:fs';
+import { dirname } from 'node:path';
+import { globSync } from 'glob';
+import Markdoc, { Config } from '@markdoc/markdoc';
+
+
+export function convertFile(filePath: string, config: Config = {}) {
+    config = { ...defaultConfig, ...config }
+    let file = readFileSync(filePath, { encoding: 'utf8', flag: 'r' }).split('\n');
+
+    let ast = Markdoc.parse(file.join('\n'));
+    let errors = Markdoc.validate(ast, config);
+    if (errors.length !== 0) {
+      // Resolve end* to /* first
+      resolveClosingTag(errors, file);
+    }
+
+    ast = Markdoc.parse(file.join('\n'));
+    errors = Markdoc.validate(ast, config);
+    if (errors.length !== 0) {
+      // Resolve self-enclosing tags
+      resolveSelfEnclosingTag(errors, file);
+    }
+
+    ast = Markdoc.parse(file.join('\n'));
+    errors = Markdoc.validate(ast, config);
+
+    file = extractTitleToFrontmatter(ast).split('\n');
+
+    return { file, errors }
+}
+
+async function gbmd_mdoc(options: { input: string; output: string }, config: Config = defaultConfig) {
+
+  const inputPath = options.input;
+  if (!inputPath) throw new Error('Input Path is empty')
+
+  const outputPath = options.output;
+  if (!outputPath) throw new Error('Output Path is empty')
+
+  let data = "";
+  if (!process.stdin.isTTY) {
+    for await (const chunk of process.stdin) data += chunk
+  }
+
+  const filePaths = [...globSync(`${inputPath}/**/*.md`), ...data.split('\n') ];
+
+  for (const filePath of filePaths) {
+    if (!filePath){
+      continue;
+    }
+
+    let {file, errors} = convertFile(filePath, config);
+
+    if (errors.length !== 0) {
+      process.stderr.write(filePath)
+      process.stderr.write(file.join('\n'))
+      process.stderr.write(JSON.stringify(errors, undefined, 2))
+    }
+    else {
+      const dataOutput = new Uint8Array(Buffer.from(file.join('\n')))
+      mkdirSync(`${outputPath}/${dirname(filePath.replace(inputPath, ''))}`, {recursive: true});
+      writeFileSync(`${outputPath}/${filePath.replace(inputPath, '').replace(/\.md$/, '.mdoc')}`, dataOutput)
+    }
+  }
+}
 
 program
   .name('gbmd-mdoc')
